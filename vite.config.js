928516be917fd28c89eabc54d52089479916b630
sneha <sneha.naik1117@gmail.com>
2026-04-06 12:43:01 +0530

    README Generator with gitignore

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/sneh1117/Readme_Gen",  // ← replace with your exact GitHub repo name
   plugins: [react()],
   server: {
     proxy: {
